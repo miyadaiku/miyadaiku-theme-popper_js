0b7508f43ac14f68530c051fa7b14e0ff70da320
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-15 09:10:06 +0900

    heh

--- miyadaiku_theme_popper_js/__init__.py
+++ miyadaiku_theme_popper_js/__init__.py
@@ -1,5 +1,4 @@
-from miyadaiku.core.contents import bin_loader
-from miyadaiku.core import config
+import pkg_resources
 
 from . __version__ import __version__
 
@@ -12,8 +11,8 @@ def load_package(site):
     popper_js = POPPER_JS_MIN if f else POPPER_JS
     src_path = 'externals/'+popper_js
     
-    content = bin_loader.from_package(site, __name__, src_path, DEST_PATH+popper_js)
-    site.contents.add(content)
+    content = pkg_resources.resource_string(__name__, src_path)
+    site.files.add_bytes("binary", DEST_PATH + popper_js, content )
     site.config.add('/', {'popper_js_path': DEST_PATH+popper_js})
 
-    site.add_template_module('popper_js', 'miyadaiku.themes.popper_js!macros.html')
+    site.add_template_module('popper_js', 'miyadaiku_theme_popper_js!macros.html')
--- setup.py
+++ setup.py
@@ -3,25 +3,24 @@ import re
 import pathlib
 import distutils
 from setuptools import setup, find_packages
-from miyadaiku.common import setuputils
+from miyadaiku import setuputils
 
 DIR = pathlib.Path(__file__).resolve().parent
-os.chdir(DIR)
 
 requires = [
     "miyadaiku"
 ]
 
-srcdir = 'node_modules/popper.js/dist/umd/'
-destdir = 'miyadaiku/themes/popper_js/externals/'
+srcdir = DIR / 'node_modules/popper.js/dist/umd/'
+destdir = DIR / 'miyadaiku_theme_popper_js/externals/'
 copy_files = [[srcdir, ['popper*.js'], destdir]]
 
-versionpy = DIR / 'miyadaiku/themes/popper_js/__version__.py'
+versionpy = DIR / 'miyadaiku_theme_popper_js/__version__.py'
 version = re.search(r'"([\d.]+)"', versionpy.read_text())[1]
 
 
 setup(
-    name="miyadaiku.themes.popper_js",
+    name="miyadaiku_theme_popper_js",
     version=version,
     author="Atsuo Ishimoto",
     license="MIT",
@@ -32,12 +31,12 @@ setup(
     ],
     description='Popper.js files for miyadaiku static site generator',
     long_description=setuputils.read_file(DIR, 'README.rst'),
-    url='https://github.com/miyadaiku/miyadaiku-themes-popper_js',
+    url='https://github.com/miyadaiku/miyadaiku_theme_popper_js',
     project_urls={
         'Miyadaiku': 'https://miyadaiku.github.io/',
     },
 
-    packages=list(setuputils.list_packages(DIR, 'miyadaiku')),
+    packages=list(setuputils.list_packages(DIR, 'miyadaiku_theme_popper_js')),
     package_data={
         '': setuputils.SETUP_FILE_EXTS,
     },
